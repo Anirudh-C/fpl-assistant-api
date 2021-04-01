b01ae485a344e2be3c456e6f213a72285d0cecf2
Anirudh C <anirudh.c@iiitb.org>
2021-04-01 21:27:50 +0530

    add: login endpoint with key cookie

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ import asyncio
 from fpl import FPL
 
 import flask
-from flask import jsonify, request
+from flask import jsonify, request, make_response
+
+from cryptography.fernet import Fernet
 
 app = flask.Flask("FPL Assistant API")
 
@@ -25,6 +27,32 @@ def teams():
         team_id = int(request.args["team"])
         return jsonify(asyncio.run(get_squad(team_id)))
     return jsonify({ "status": "Error: Specify team index" })
+
+async def fpl_login(email: str, password: str):
+    async with aiohttp.ClientSession() as session:
+        fpl = FPL(session)
+        await fpl.login(email, password)
+
+@app.route("/api/login", methods=["POST"])
+def login():
+    try:
+        username = request.form["login"]
+        password = request.form["password"]
+        asyncio.run(fpl_login(username, password))
+        key = Fernet.generate_key()
+        fernet = Fernet(key)
+        enc_pass = fernet.encrypt(password.encode())
+        del password
+        del fernet
+        response = make_response({ "status": "Success" })
+        response.set_cookie("key", key, secure=True)
+        del key
+        return response
+    except ValueError:
+        response = make_response({ "status": "Incorrect Credentials!" }, 401)
+        return response
+    except:
+        return make_response({ "status": "Failure!" })
 
 def serve():
     return app
