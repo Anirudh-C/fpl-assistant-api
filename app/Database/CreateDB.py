98e75d392e8825e4b529038c88819c4442af6418
gandharv <gandharv.suri@iiitb.org>
2021-04-23 14:26:16 +0530

    Database Update

--- app/Database/CreateDB.py
+++ app/Database/CreateDB.py
@@ -1,17 +1,15 @@
 # Basic imports
-import os
 
 # DB imports
 from sqlalchemy import create_engine
 from sqlalchemy import text
 
-# FPL API wrapper imports
-import aiohttp
-import asyncio
 
-def run_file(sql_file, engine):
+def create_db(sql_file, engine):
+    run_database_file(sql_file, engine)
+
+def run_database_file(sql_file, engine):
     sql_file = open(sql_file,'r')
-    session = engine.connect()
     # Create an empty command string
     sql_command = ''
 
@@ -26,8 +24,7 @@ def run_file(sql_file, engine):
             if sql_command.endswith(';'):
                 # Try to execute statement and commit it
                 try:
-                    session.execute(text(sql_command))
-                    # session.commit()
+                    engine.execute(text(sql_command))
 
                 # Assert in case of error
                 except:
@@ -38,14 +35,8 @@ def run_file(sql_file, engine):
                     sql_command = ''
 
 if __name__ == "__main__":
-    # # DB connection ENV variables
-    # db_host = os.environ.get("DB_HOST")
-    # db_user = os.environ.get("DB_USERNAME")
-    # db_pass = os.environ.get("DB_PASS")
-    # db_name = "FPL"
-    # engine = create_engine(
-        # "mysql+pymysql://{}:{}@{}/{}".format(db_user, db_pass, db_host, db_name))
     engine = create_engine("mysql+pymysql://root:pass@localhost:3600")
-    run_file("schema.sql", engine)
+    engine = engine.connect()
+    create_db("schema.sql", engine)
 
 
